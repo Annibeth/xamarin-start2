da3be0bfdba01a082121207667f99fd1a57304d8
Admin <Admin>
2016-11-24 10:13:21 +0100

    med apps

---
+++ Lommeregner/Lommeregner/Lommeregner/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Lommeregner
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+                        
+        }
+        private void ButtonClickec(Object sender, EventArgs e)
+        {
+            var senderButton = sender as Button;
+            topLabel.Text = senderButton.Text;
+                
+        }
+
+    }
+}
---
+++ XamlSampels/XamlSampels/XamlSampels/HelloXamlPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamlSampels
+{
+    public partial class HelloXamlPage : ContentPage
+    {
+        public HelloXamlPage()
+        {
+            InitializeComponent();
+            
+            
+        }
+
+        void OnSliderValueChanged(object sender, ValueChangedEventArgs args)
+        {
+            //valueLabel.Text = args.NewValue.ToString("F3");
+            valueLabel.Text = ((Slider)sender).Value.ToString("F3");
+
+        }
+
+        async void OnButtonClicked(object sender, EventArgs args)
+        {
+            Button button = (Button)sender;
+            await DisplayAlert("Clicked!",
+                "The button labeled '" + button.Text + "' has been clicked",
+                "OK");
+        }
+
+    }
+}
